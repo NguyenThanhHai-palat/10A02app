77d59174b9f171f01248e76ce1a82139e95f974b
Hải Nguyễn Thanh <nguyenthanhhai12072008@gmail.com>
2024-01-12 16:02:53 +0700

    update1.1

---
+++ mobile/mobile/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using mobile.Models;
+using mobile.ViewModels;
+using mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobile.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
